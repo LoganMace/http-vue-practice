83d157a30f693df571fcc75f4baf5bb0bd1bcdf2
Logan Mace <lmmace92@hotmail.com>
2018-10-11 09:19:43 -0500

    finished

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import App from './App.vue';
+
+Vue.use(VueResource);
+Vue.http.options.root = 'https://vue-max-course.firebaseio.com/';
+Vue.http.interceptors.push((request, next) => {
+  console.log('request: ', request);
+  if(request.method == "POST") {
+    request.method = "PUT";
+  }
+  next(response => {
+    response.json = () => { return {messages: response.body }}
+  });
+});
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
